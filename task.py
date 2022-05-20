3d239225f6f5df5e20a13843bd593f6c00eba374
EconLQ <zavaliybogdan@gmail.com>
2022-05-20 13:18:33 +0300

    Uppdated break on exception when input is invalid.

--- task.py
+++ task.py
@@ -24,7 +24,8 @@ def get_geoname_id(city: str) -> str:
     geoname id is identifier in the GeoNames geographical database
     """
     g = geocoder.geonames(f"{city}", key="econlq", maxRows=2)
-    return str(g.geonames_id)
+    if g.geonames_id is not None:
+        return str(g.geonames_id)
 
 
 def input_parser():
@@ -32,7 +33,7 @@ def input_parser():
     Parse through command line arguments
     """
     parser = argparse.ArgumentParser(description="Parse names of the cities")
-    parser.add_argument('c', type=str, help="name of the city")
+    parser.add_argument('c', type=str, help="name of the city", nargs="+")
     return parser.parse_args()
 
 
@@ -46,11 +47,13 @@ def data_about_city(city: str) -> str:
     response = requests.request("GET", url, headers=HEADERS,
                                 params=QUERYSTRING)
 
-    data.update(response.json())
+    static_city_data.update(response.json())
     write_data("static_city_data.json", data=response.json())
-
-    return f"--------------\n{data.get('name')}\n\n{data.get('country').get('name')}" \
-                        f"\n{data.get('currency').get('code')}\n{data.get('population')}\n=============="
+    if isinstance(get_geoname_id(city), str):
+        return f"--------------\n{static_city_data.get('name')}\n\n{static_city_data.get('country').get('name')}" \
+                            f"\n{static_city_data.get('currency').get('code')}\n{static_city_data.get('population')}\n=============="
+    else:
+        return f"--------------\n{user_input[0]}\n\nInvalid city name: {user_input[0]}\n=============="
 
 
 def invalid_city_name_exception(user_input: str):
@@ -59,10 +62,9 @@ def invalid_city_name_exception(user_input: str):
     the status key value
     """
     try:
-        if data.get("status") == "success":
-            pass
-        elif data.get("status") == "failed":
-            print(f"--------------\n{user_input}\n\nInvalid city name: {user_input}\n==============")
+        if static_city_data.get("status") == "failed":
+            print(f"--------------\n{user_input[0]}\n\nInvalid city name: "
+                  f"{user_input[0]}\n==============")
     except AttributeError:
         print(f"--------------\nSystem Error\n==============")
 
@@ -75,8 +77,8 @@ def append_cities_to_file(url: str):
     response = requests.request("GET", url, headers=HEADERS,
                                 params=QUERYSTRING)
 
-    data_2.append(response.json())
-    return write_data("cities_test.json", data_2)
+    dynamic_cities_data.append(response.json())
+    return write_data("cities_test.json", dynamic_cities_data)
 
 
 def is_there_more_than_two_cities(name_city: str) -> dict:
@@ -85,12 +87,11 @@ def is_there_more_than_two_cities(name_city: str) -> dict:
     city with the same name. If the country is the same, then remove duplicated
     country and returns dict with unique keys (geoname_id) and values (country)
     """
-    g = geocoder.geonames(f"{name_city}", key="econlq", maxRows=5)
-    append_list = {}
-    for result in g:
-        append_list.update({result.geonames_id: result.country})
+    g = geocoder.geonames(f"{name_city}", key=keys.get("geonames_api_key"),
+                          maxRows=5)
+    append_list = {result.geonames_id: result.country for result in g}
     temp = []
-    final_result = dict()
+    final_result = {}
     for key, value in append_list.items():
         if value not in temp:
             temp.append(value)
@@ -99,37 +100,35 @@ def is_there_more_than_two_cities(name_city: str) -> dict:
 
 
 if __name__ == '__main__':
-    data = read_data("static_city_data.json")
-    data_2 = read_data("dynamic_cities_data.json")
+    static_city_data = read_data("static_city_data.json")
+    dynamic_cities_data = read_data("dynamic_cities_data.json")
+    keys = read_data("keys.json")
     arg = input_parser()
     user_input = arg.c  # c -> name of the city
     QUERYSTRING = {"format": "json", "language": "en"}
     HEADERS = {
         "X-RapidAPI-Host": "world-geo-data.p.rapidapi.com",
-        "X-RapidAPI-Key": "25fae88ae7mshfd287451137fc8dp10272ajsn3c85f02d7dd0"
-    } # includes World Geo Data API host destination and my API key
+        "X-RapidAPI-Key": keys.get("X-RapidAPI-Key")
+    }
     try:
         geonames_ids = [] # list where geoname ids of two cities will be written
         if len(is_there_more_than_two_cities(str(user_input))) == 2:
             cities = is_there_more_than_two_cities(user_input)
-            for geoname_id in cities.keys():
-                geonames_ids.append(geoname_id)
-
+            geonames_ids.extend(iter(cities.keys()))
             for g_id in geonames_ids:
                 url = f"https://world-geo-data.p.rapidapi.com/cities/{str(g_id)}"
                 append_cities_to_file(url)
                 # call time.sleep for 3 sec, as my API provider doesn't allow
                 # more than 1 request/second
                 time.sleep(3)
-                for l in data_2:
+                for l in dynamic_cities_data:
                     print(
                         f"--------------\n{l.get('name')}\n\n{l.get('country').get('name')}" \
                         f"\n{l.get('currency').get('code')}\n{l.get('population')}\n==============")
         else:
             print(data_about_city(user_input))
     except (AttributeError, KeyboardInterrupt):
-        invalid_city_name_exception(user_input)
+        print(f"--------------\nSystem Error\n==============")
     # writing empty list, so when run the file next time
-    # we will have only two dictionaries in a list,
-    # where we can get the requested data
+    # we will have only two dictionaries in a list, where we can get the requested data
     write_data("dynamic_cities_data.json", [])
